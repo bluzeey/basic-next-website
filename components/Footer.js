e3ae420f3c780fb56ae36743da32072c1389b7dd
Sahil Maheshwari <sahilm1711@gmail.com>
2021-11-12 14:33:28 +0530

    Creating a nextjs template

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <p>Copyrights 2021 Members List</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <h1>Members List</h1>
+            </div>
+            <Link href="/"><a>Home</a></Link>
+            <Link href="/about"><a>About</a></Link>
+        </nav>
+    )
+}
+export default Navbar
