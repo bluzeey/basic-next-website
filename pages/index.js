a22e371991afa4a9a3c60210bd1b570b2f296317
Sahil Maheshwari <sahilm1711@gmail.com>
2021-11-12 14:38:17 +0530

    Added the layout settings

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,8 @@ import Navbar from '../components/Navbar'
 export default function Home() {
   return (
     <div className={styles.container}>
-     <Navbar/>
      <h1>HomePage</h1>
      <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.</p>
-     <Footer/>
     </div>
 
   )
