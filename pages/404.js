22f47d7c142918ff5af9b12f62e7aebecabb6d79
Sahil Maheshwari <sahilm1711@gmail.com>
2021-11-14 13:47:50 +0530

    updating 404

--- pages/404.js
+++ pages/404.js
@@ -1,26 +1,25 @@
-import React from 'react'
 import Link from 'next/link'
-import {useEffect} from 'react'
-import {useRouter} from 'next/router'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
+const NotFound = () => {
+  const router = useRouter()
 
-const notFound = () => {
-    useEffect(()=>{
-    const router=useRouter()
-        setTimeout(()=>{
-            //router.go(1)
-            router.push('/');
-        },3000)
-    },[])
+  useEffect(() => {
+    setTimeout(() => {
+      // router.go(-1)
+      // router.go(1)
+      router.push('/')
+    }, 3000)
+  }, [])
 
-    
-    return (
-        <div className="not-found">
-            <h1>Ooops....</h1>
-            <h2>That page cannot be found.</h2>
-            <p>Go back to the <Link href="/"><a>HomePage</a></Link></p>
-        </div>
-    )
+  return (
+    <div className="not-found">
+      <h1>Ooops...</h1>
+      <h2>That page cannot be found :(</h2>
+      <p>Going back to the <Link href="/"><a>Homepage</a></Link> is 3 seconds...</p>
+    </div>
+  );
 }
-
-export default notFound
+ 
+export default NotFound;
